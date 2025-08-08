cbf93af1e51dd31b87e22c4ce5d0adf526e9c3b6
Jamie Frost <jamcris11@hotmail.co.uk>
2025-08-08 18:13:07 +0100

    Alt: markdown.py refactor

--- markdown.py
+++ markdown.py
@@ -1,3 +1,9 @@
+def __generate_markdown_hugo_summary_start(dirname):
+    return '{{< details summary="{d}" >}}'.replace("{d}", dirname)
+
+def __generate_markdown_hugo_summary_end():
+    return '{{< /details >}}'
+
 def _generate_markdown_variable(unit, root):
     name = unit["name"]
     title = f'## variable {name}' if root["kind"] == "file" else ""
@@ -6,7 +12,9 @@ def _generate_markdown_variable(unit, root):
     if root["kind"] == "file":
         variable_definition = unit["variable_definition"]
     else:
-        variable_definition = unit["variable_definition"].replace(root["name"] + "::", "")
+        variable_definition = unit["variable_definition"].replace(
+                root["name"] + "::", ""
+        )
 
     return (
         f'{title}\n'
@@ -21,13 +29,14 @@ def _generate_markdown_enum(unit):
     name = unit["name"]
     enums_formatted = ',\n\t'.join(unit["enum_values"])
     return (
-        f'## enum - {name}\n'
-        f'```c\n'
-        f'enum {name} {{\n'
-        f'\t{enums_formatted}\n'
-        f'}}\n'
-        f'```\n'
-        f'---\n\n'
+        f'## enum - {name}   \n'
+        f'```c               \n'
+        f'enum {name} {{     \n'
+        f'  {enums_formatted}\n'
+        f'}}                 \n'
+        f'```                \n'
+        f'---                \n'
+        f'                   \n'
     )
 
 def _generate_markdown_function_params(unit):
@@ -37,10 +46,14 @@ def _generate_markdown_function_params(unit):
         return params
 
     for param in unit:
-        params += "-\t\n"
-        params += "\t```c\n"
-        params += "\t" + param["type"] + " " + param["name"] + "\n"
-        params += "\t```\n"
+        _type = param["type"]
+        _name = param["name"]
+        params += (
+            f'-\t\n'
+            f'  ```c           \n'
+            f'  {_type} {_name}\n'
+            f'  ```            \n'
+        )
 
     return (
         f'### parameters\n\n'
@@ -54,28 +67,35 @@ def _generate_markdown_function(unit):
     description = unit["description"]
     params_formatted = _generate_markdown_function_params(unit["params"])
     return_type = unit["return_type"]
-    return_description = unit["return_description"] if "return_description" in unit else ""
+    return_description = (
+            unit["return_description"] 
+            if "return_description" in unit else 
+            ""
+    )
+    
     return (
-        f'## function - {name}\n\n'
-        f'{description}\n'
-        f'```c\n'
-        f'{definition} {args}\n'
-        f'```\n\n'
-        f'{params_formatted}\n'
-        f'### returns\n'
-        f'```c\n'
-        f'{return_type}\n'
-        f'```\n'
-        f'{return_description}\n\n'
-        f'---\n'
+        f'## function - {name}\n'
+        f'{description}       \n'
+        f'```c                \n'
+        f'{definition} {args} \n'
+        f'```                 \n'
+        f'                    \n'
+        f'{params_formatted}  \n'
+        f'### returns         \n'
+        f'```c                \n'
+        f'{return_type}       \n'
+        f'```                 \n'
+        f'{return_description}\n'
+        f'                    \n'
+        f'---                 \n'
     )
 
 def _generate_markdown_includes(includes):
     includes = '\n'.join([ '#include "' + x + '"' for x in includes ])
     return (
-        f'```c\n'
+        f'```c      \n'
         f'{includes}\n'
-        f'```\n'
+        f'```       \n'
     )
 
 def generate_markdown(data: dict):
@@ -83,7 +103,7 @@ def generate_markdown(data: dict):
 
     markdown_units.append("# " + data["kind"] + " - " + data["name"] + "\n")
 
-    if "includes" in data:
+    if data["includes"]:
         markdown_units.append(_generate_markdown_includes(data["includes"]))
 
     for _, v in data["defs"].items():
@@ -95,21 +115,12 @@ def generate_markdown(data: dict):
             case "variable":
                 markdown = _generate_markdown_variable(v, data)
             case _:
-                print("undefined markdown behavior for kind " + v["kind"])
+                markdown = "undefined markdown behavior for kind " + v["kind"]
 
         markdown_units.append(markdown)
 
     return "\n".join(markdown_units)
 
-def _generate_markdown_treeview_dir_hugo_summary_start(dirname):
-    return (
-        f'{{{{< details summary="{dirname}" >}}}}'
-    )
-
-def _generate_markdown_treeview_dir_hugo_summary_end():
-    return (
-        '{{< /details >}}'
-    )
 
 def generate_markdown_treeview(project_name: str, directories):
     markdown_units = []
@@ -119,14 +130,14 @@ def generate_markdown_treeview(project_name: str, directories):
         f'---\n'
     ))
 
-    #print(files)
-
     for d in directories:
         dirname = d["name"]
-        markdown_units.append(_generate_markdown_treeview_dir_hugo_summary_start(dirname))
+        markdown_units.append(__generate_markdown_hugo_summary_start(dirname))
         for refid,name in d["files"].items():
-            markdown_units.append(f'- [{name}](/{project_name}/{dirname}/{name}/)')
-        markdown_units.append(_generate_markdown_treeview_dir_hugo_summary_end())
+            markdown_units.append(
+                    f'[{name}](/{project_name}/{dirname}/{name}/)'
+            )
+        markdown_units.append(__generate_markdown_hugo_summary_end())
 
     return "\n".join(markdown_units)
 
